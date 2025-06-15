9bf3c0b898ae27d3ad29aff857fda14ab48fb3e9
Rômulo Fernandes Evangelista <romfe89@proton.me>
2025-06-15 14:42:20 -0300

    added some security

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     namespace = "com.romulo.cotidiano"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
+
     dependenciesInfo {
         includeInApk = false
         includeInBundle = false
@@ -35,20 +36,35 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
+
     signingConfigs {
-    create("release") {
-        keyAlias = keystoreProperties.getProperty("keyAlias")
-        keyPassword = keystoreProperties.getProperty("keyPassword")
-        storeFile = file(keystoreProperties.getProperty("storeFile"))
-        storePassword = keystoreProperties.getProperty("storePassword")
+        create("release") {
+            keyAlias = keystoreProperties.getProperty("keyAlias")
+            keyPassword = keystoreProperties.getProperty("keyPassword")
+            storeFile = file(keystoreProperties.getProperty("storeFile"))
+            storePassword = keystoreProperties.getProperty("storePassword")
+        }
     }
-}
+
+    flavorDimensions += "version"
+
+    productFlavors {
+        create("dev") {
+            dimension = "version"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+        }
+        create("prod") {
+            dimension = "version"
+        }
+    }
 
 buildTypes {
     getByName("release") {
         signingConfig = signingConfigs.getByName("release")
         isMinifyEnabled = false
         isShrinkResources = false
+        isDebuggable = false
         proguardFiles(
             getDefaultProguardFile("proguard-android-optimize.txt"),
             "proguard-rules.pro"
